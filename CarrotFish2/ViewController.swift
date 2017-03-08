1b336966c6ef65b4621c98f0c656ae6277557f7d
Nick Duckworth <nickyducky@outlook.com>
2017-03-08 18:49:47 +0000

    My first Commit

--- CarrotFish2/ViewController.swift
+++ CarrotFish2/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TheLabel: UILabel!
+    
+    var TapCount = 0
+    
+    // Push Button 1
+    @IBAction func PushButton(_ sender: Any) {
+        TheLabel.text = "Hello World"
+        TapCount = TapCount + 1
+        print(TapCount)
+        if TapCount >= 10 {
+            TheLabel.text = "Stop Tapping!!"
+        }
+    }
+    
+    // Push Button 2
+    @IBAction func PushButton2(_ sender: Any) {
+        
+        TheLabel.text = "Buttons Are Cool!!!"
+        
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.yellow
+        
     }
 
     override func didReceiveMemoryWarning() {
